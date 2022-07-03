99858c7f64090b87962b1d1c22b02183a359e189
Pradeep Makaju <makaju.pradeep87@gmail.com>
2022-07-02 20:37:27 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Hello greeting={'Hello' + 'React Ninja'}/>
   </React.StrictMode>
 );
 
