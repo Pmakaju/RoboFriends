f4cdb14233ee6e3414e155394825902a7e5880cf
Pradeep Makaju <makaju.pradeep87@gmail.com>
2022-07-12 09:43:11 -0500

    Deployment

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Scroll = (props) => {
-	return (
-		<div style={{ overflowY: 'scroll', border: '5px solid black', height: '500px' }}>
-			{props.children}
-		</div>
-		)
+  return (
+    <div style={{ overflow: 'scroll', border: '5px solid black', height: '800px'}}>
+      {props.children}
+    </div>
+  );
 };
 
 export default Scroll;
